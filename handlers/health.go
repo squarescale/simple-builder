84c27a17fdac0838a0d0152683efb2d09be5b994
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2017-04-05 18:13:47 +0200

    First version of the web service.
    Inspired from the hashiapp from https://github.com/kelseyhightower/hashiapp

---
+++ handlers/health.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"sync"
+)
+
+type healthHandler struct {
+	health interface{}
+	status *int
+	lock   sync.Locker
+}
+
+func (h *healthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+	w.WriteHeader(*h.status)
+	json.NewEncoder(w).Encode(h.health)
+}
+
+func HealthHandler(health interface{}, status *int, lock sync.Locker) http.Handler {
+	lock.Lock()
+	defer lock.Unlock()
+	if *status == 0 {
+		*status = http.StatusOK
+	}
+	return &healthHandler{
+		health: health,
+		status: status,
+		lock:   lock,
+	}
+}
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/braintree/manners"
+	"github.com/squarescale/simple-builder/handlers"
+)
+
+const version = "1.0.0"
+
+var Health struct {
+	Lock   sync.Mutex
+	Status int
+}
+
+func GetenvDef(name, def string) string {
+	res := os.Getenv(name)
+	if res == "" {
+		res = def
+	}
+	return res
+}
+
+func main() {
+	log.Println("Starting Simple Builder...")
+
+	httpAddr := os.Getenv("NOMAD_ADDR_http")
+	if httpAddr == "" {
+		httpAddr = "localhost:80"
+	}
+
+	flag.StringVar(&httpAddr, "http", httpAddr, "Listening address")
+	flag.Parse()
+
+	log.Printf("HTTP service listening on %s", httpAddr)
+
+	mux := http.NewServeMux()
+	mux.Handle("/version", handlers.VersionHandler(version))
+	mux.Handle("/health", handlers.HealthHandler(&Health, &Health.Status, &Health.Lock))
+
+	httpServer := manners.NewServer()
+	httpServer.Addr = httpAddr
+	httpServer.Handler = handlers.LoggingHandler(mux)
+
+	errChan := make(chan error, 10)
+
+	go func() {
+		errChan <- httpServer.ListenAndServe()
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	for {
+		select {
+		case err := <-errChan:
+			if err != nil {
+				log.Fatal(err)
+			}
+		case s := <-signalChan:
+			log.Println(fmt.Sprintf("Captured %v. Exiting...", s))
+			httpServer.BlockingClose()
+			os.Exit(0)
+		}
+	}
+}
