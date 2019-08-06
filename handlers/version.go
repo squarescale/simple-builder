0f3760447b90b85c0607bcba3dc2dc0ecdb864d9
Jérôme Renard <jerome.renard@squarescale.com>
2019-08-06 12:23:26 +0200

    Simplify handlers.VersionHandler

--- handlers/version.go
+++ handlers/version.go
@@ -5,21 +5,23 @@ import (
 	"net/http"
 )
 
-type VersionResponse struct {
-	Version string `json:"version"`
-}
-
 type versionHandler struct {
 	version string
 }
 
 func (h *versionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	response := VersionResponse{
-		Version: h.version,
-	}
-	json.NewEncoder(w).Encode(response)
-	return
+	w.Header().Set(
+		"Content-Type",
+		"application/json",
+	)
+
+	json.NewEncoder(w).Encode(
+		struct {
+			Version string `json:"version"`
+		}{
+			Version: h.version,
+		},
+	)
 }
 
 func VersionHandler(version string) http.Handler {
